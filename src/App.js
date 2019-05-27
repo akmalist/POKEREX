73b1545eb90171e953fd3bf8759bec585750b54e
Aki <akmalist@gmail.com>
2019-05-26 23:17:19 -0700

    v2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import Pokedex from './Pokedex';
-import Pokecard from './Pokecard';
+import Pokegame from './Pokegame';
+// import Pokedex from './Pokedex';
+// import Pokecard from './Pokecard';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Pokecard id={7} name='Charmander' type='fire' base_experience={62} />
-        <Pokedex />
+        {/* <Pokecard id={7} name='Charmander' type='fire' base_experience={62} /> */}
+        {/* <Pokedex /> */}
+        <Pokegame />
       </div>
     );
   }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import './Pokecard.css';
 
-const imgApi =
-  ' https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const imgApi = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
 class Pokecard extends Component {
   render() {
-    const pokImage = `${imgApi}${this.props.id}.png`;
+    function pad(n) {
+      return ('000000' + n).slice(-3);
+    }
+    const pokImage = `${imgApi}${pad(this.props.id)}.png`;
 
+    const { name, type, base_experience } = this.props;
     return (
-      <div>
-        <h1>Pokecard</h1>
-        <img src={pokImage} />
+      <div className='Pokecard'>
+        <h1 className='Pokecard-title'>{name}</h1>
+        <div className='Pokecard-image'>
+          <img src={pokImage} alt={name} />
+        </div>
+        <div className='Pokecard-text'> Type: {type}</div>
+        <div className='Pokecard-text'> Exp : {base_experience}</div>
       </div>
     );
   }
